f7a97e170cefefe3ab54f3203d64c057909ff453
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-04-26 17:04:10 +0300

    Viewpager for promo intro, Lottie animation

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LogInOrSignUpFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LogInOrSignUpFragment.java
@@ -1,15 +1,23 @@
 package org.wordpress.android.ui.accounts.login;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import org.wordpress.android.R;
+import org.wordpress.android.ui.accounts.JetpackCallbacks;
+import org.wordpress.android.widgets.WPViewPager;
+
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import org.wordpress.android.R;
-import org.wordpress.android.ui.accounts.JetpackCallbacks;
+import android.widget.TextView;
 
 public class LogInOrSignUpFragment extends Fragment {
 
@@ -55,7 +63,87 @@ public class LogInOrSignUpFragment extends Fragment {
             }
         });
 
+        WPViewPager pager = (WPViewPager) view.findViewById(R.id.intros_pager);
+        MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout_indicator);
+        tabLayout.setupWithViewPager(pager, true);
+
         return view;
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return LottieFragment.newInstance("data.json", getString(R.string.login_promo_text_onthego));
+                case 1:
+                    return LottieFragment.newInstance("data.json", getString(R.string.login_promo_text_realtime));
+                case 2:
+                    return LottieFragment.newInstance("data.json", getString(R.string.login_promo_text_anytime));
+                case 3:
+                    return LottieFragment.newInstance("data.json", getString(R.string.login_promo_text_notifications));
+                case 4:
+                    return LottieFragment.newInstance("data.json", getString(R.string.login_promo_text_jetpack));
+                default:
+                    return null;
+            }
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+
+    public static class LottieFragment extends Fragment {
+        private final static String KEY_ANIMATION_FILENAME = "KEY_ANIMATION_FILENAME";
+        private final static String KEY_PROMO_TEXT = "KEY_PROMO_TEXT";
+
+        private String mAnimationFilename;
+        private String mPromoText;
+
+        static LottieFragment newInstance(String animationFilename, String promoText) {
+            LottieFragment fragment = new LottieFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(KEY_ANIMATION_FILENAME, animationFilename);
+            bundle.putString(KEY_PROMO_TEXT, promoText);
+            fragment.setArguments(bundle);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            mAnimationFilename = getArguments().getString(KEY_ANIMATION_FILENAME);
+            mPromoText = getArguments().getString(KEY_PROMO_TEXT);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
+                savedInstanceState) {
+            ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.login_intro_template_view, container, false);
+
+            TextView promoText = (TextView) rootView.findViewById(R.id.promo_text);
+            promoText.setText(mPromoText);
+
+            LottieAnimationView lottieAnimationView = (LottieAnimationView) rootView.findViewById(R.id.animation_view);
+            lottieAnimationView.setAnimation(mAnimationFilename, LottieAnimationView.CacheStrategy.Weak);
+            lottieAnimationView.setImageAssetsFolder("images");
+
+            return rootView;
+        }
     }
 
     @Override
